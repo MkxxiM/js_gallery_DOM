1b7cbd6222ac966196626a88c409ed52595ba8fd
Your Name <you@example.com>
2026-09-03 03:29:22 +0300

    Solution_v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const bigPicture = document.querySelector('.gallery__large-img');
+const bigPictureLink = bigPicture.getAttribute('src');
+
+gallery.addEventListener('click', () => {
+  const picture = event.target.closest('.gallery__img');
+  const pictureLink = picture.getAttribute('src');
+
+  if (!picture) {
+    return null;
+  } else {
+    bigPicture.gallery.replaceChild(picture, bigPicture);
+
+    const temp = bigPictureLink;
+
+    bigPicture.setAttribute('src', pictureLink);
+    picture.setAttribute('src', temp);
+  }
+});
