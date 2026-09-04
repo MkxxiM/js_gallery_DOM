ff4e717d223d5a4182607deae6152d47a76f3fd6
Your Name <you@example.com>
2026-09-04 13:02:12 +0300

    make build

---
+++ js_gallery_DOM.2861e03f.js
@@ -0,0 +1,2 @@
+"use strict";var list=document.querySelector(".gallery__list");list.addEventListener("click",function(e){e.preventDefault();var t=e.target.closest(".list-item").querySelector("a");document.querySelector("#largeImg").src=t.href});
+//# sourceMappingURL=js_gallery_DOM.2861e03f.js.map
